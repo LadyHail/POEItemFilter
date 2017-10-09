cc4f1573d690403fef9a4dcb956302e1e5841f51
LadyHail <ladyhail@outlook.com>
2017-10-09 12:23:41 +0200

    Add project files.

---
+++ POEItemFilter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="POEItemFilter.MvcApplication" Language="C#" %>
---
+++ POEItemFilter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(POEItemFilter.Startup))]
+namespace POEItemFilter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
