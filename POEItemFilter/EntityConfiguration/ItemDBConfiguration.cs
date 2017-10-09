e3e5c13149c4e9989016b8f4a8a6ab395be14661
LadyHail <ladyhail@outlook.com>
2017-10-09 16:04:45 +0200

    Add entity type configuration.

---
+++ POEItemFilter/EntityConfiguration/ItemDBConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using POEItemFilter.Models.ItemsDB;
+
+namespace POEItemFilter.EntityConfiguration
+{
+    public class ItemDBConfiguration : EntityTypeConfiguration<ItemDB>
+    {
+        public ItemDBConfiguration()
+        {
+            Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            Property(i => i.Level)
+                .IsOptional();
+
+            Property(i => i.BaseType)
+                .IsRequired();
+
+            Property(i => i.Type)
+                .IsRequired();
+
+            Property(i => i.Attribute1)
+                .IsOptional();
+
+            Property(i => i.Attribute2)
+                .IsOptional();
+        }
+    }
+}
