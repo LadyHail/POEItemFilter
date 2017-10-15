c1bea4028c96dcddd8c85e45eeaa8d0c3f3f5919
LadyHail <ladyhail@outlook.com>
2017-10-15 22:23:11 +0200

    Repair connection database - views (items from web). Part 1 - with bugs.

--- POEItemFilter/Controllers/UsersItemsController.cs
+++ POEItemFilter/Controllers/UsersItemsController.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using POEItemFilter.Models;
+using POEItemFilter.Models.ItemsDB;
 using POEItemFilter.ViewModels;
 
 namespace POEItemFilter.Controllers
@@ -38,84 +40,100 @@ namespace POEItemFilter.Controllers
             return View(viewModel);
         }
 
-        //GET: /UsersItems/SelectBaseType/id
-        public ActionResult SelectBaseType(int id)
+        public ActionResult Refresh(string id)
         {
-            var basetypes = _context.BaseTypes.ToList();
+            string[] ids = id.Split('|');
+            int baseTypeId = int.Parse(ids[0]);
+            int typeId = int.Parse(ids[1]);
+            int attribute1Id = int.Parse(ids[2]);
+            int attribute2Id = int.Parse(ids[3]);
 
-            var types = _context.Types
-                .Where(m => m.BaseTypeId == id)
-                .Select(m => m)
-                .ToList();
+            var baseTypes = _context.BaseTypes.ToList();
+            bool isBaseTypeInDb = baseTypes.Any(i => i.Id == baseTypeId);
 
-            if (types.Count() == 0)
+            bool isTypeIdInDb = _context.Types.Any(i => i.Id == typeId);
+            var types = (List<ItemType>)null;
+
+            if (isTypeIdInDb)
             {
-                types = _context.Types.ToList();
+                types = baseTypes
+                    .SelectMany(i => i.Types)
+                    .ToList();
+            }
+            else
+            {
+                types = baseTypes
+                    .SelectMany(i => i.Types)
+                    .Where(i => i.BaseTypeId == baseTypeId)
+                    .ToList();
+
+                if (types.Count() == 0)
+                {
+                    types = baseTypes
+                    .SelectMany(i => i.Types)
+                    .ToList();
+                }
             }
 
-            var attributes = _context.Attributes
-                .Where(m => m.BaseTypeId == id)
-                .Select(m => m)
-                .ToList();
-
-            var items = _context.ItemsDB
-                .Where(i => i.BaseTypeId == id)
-                .Select(i => i)
-                .ToList();
-
-            var viewModel = new NewItemViewModel()
-            {
-                Items = items,
-                BaseTypes = basetypes,
-                Types = types,
-                Attributes = attributes
-            };
-
-            return View("NewItem", viewModel);
-        }
-
-        public ActionResult SelectType(int id)
-        {
-            var baseTypes = _context.BaseTypes.ToList();
-
-            var types = baseTypes
-                .SelectMany(i => i.Types)
-                .ToList();
-
-            var attributes = types.SingleOrDefault(i => i.Id == id).Attributes;
-
-            var items = _context.ItemsDB
-                .Where(i => i.TypeId == id)
-                .Select(i => i)
-                .ToList();
-
-            var viewModel = new NewItemViewModel()
-            {
-                BaseTypes = baseTypes,
-                Types = types,
-                Attributes = attributes,
-                Items = items
-            };
-
-            return View("NewItem", viewModel);
-        }
-
-        public ActionResult SelectAttribute(int id)
-        {
-            var baseTypes = _context.BaseTypes.ToList();
-
-            var types = baseTypes
-                .SelectMany(i => i.Types)
-                .ToList();
-
+            bool isAttr1InDb = _context.Attributes.Any(i => i.Id == attribute1Id);
+            bool isAttr2InDb = _context.Attributes.Any(i => i.Id == attribute2Id);
             var attributes = baseTypes
                 .SelectMany(i => i.Attributes)
                 .ToList();
 
-            var items = _context.Attributes
-                .Where(a => a.Id == id)
-                .SelectMany(b => b.Items)
-                .ToList();
+            var itemsList = _context.ItemsDB.ToList();
+            var items = (List<ItemDB>)null;
+            var itemsSecondFilter = (List<ItemAttribute>)null;
+
+            if (isAttr1InDb && !isAttr2InDb)
+            {
+                items = attributes
+                    .Where(a => a.Id == attribute1Id)
+                    .SelectMany(b => b.Items)
+                    .ToList();
+            }
+
+            else if (!isAttr1InDb && isAttr2InDb)
+            {
+                items = attributes
+                    .Where(a => a.Id == attribute2Id)
+                    .SelectMany(b => b.Items)
+                    .ToList();
+            }
+
+            else if (isAttr1InDb && isAttr2InDb)
+            {
+                items = attributes
+                    .Where(a => a.Id == attribute1Id)
+                    .SelectMany(b => b.Items)
+                    .ToList();
+
+                itemsSecondFilter = items
+                    .SelectMany(i => i.Attributes)
+                    .Where(i => i.Id == attribute2Id)
+                    .ToList();
+            }
+            else
+            {
+                if (isTypeIdInDb)
+                {
+                    items = itemsList
+                        .Where(i => i.TypeId == typeId)
+                        .Select(i => i)
+                        .ToList();
+                }
+                else if (isBaseTypeInDb)
+                {
+                    items = itemsList
+                        .Where(i => i.BaseTypeId == baseTypeId)
+                        .Select(i => i)
+                        .ToList();
+                }
+                else
+                {
+                    items = itemsList;
+                }
+            }
 
             var viewModel = new NewItemViewModel()
             {
