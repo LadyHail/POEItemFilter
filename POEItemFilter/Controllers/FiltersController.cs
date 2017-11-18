fae65ab6843be882847d8c2e9e272f9cc086d6e4
LadyHail <ladyhail@outlook.com>
2017-11-18 17:49:24 +0100

    Create fiter file, add items into it, save to app folder and download by user. No database connected!

--- POEItemFilter/Controllers/FiltersController.cs
+++ POEItemFilter/Controllers/FiltersController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Web.Mvc;
 using POEItemFilter.Models;
 using POEItemFilter.ViewModels;
 
@@ -29,6 +32,81 @@ namespace POEItemFilter.Controllers
             viewModel.UsersItems.Add(item);
 
             return View("NewFilter", viewModel);
+        }
+
+        [HttpPost]
+        public JsonResult SaveFilter(string filterName, string description)
+        {
+            Models.Filters.Filter newFilter = new Models.Filters.Filter();
+            if (filterName != null)
+            {
+                //Create filter model
+                newFilter.Name = filterName;
+                newFilter.Description = description;
+                newFilter.CreateDate = DateTime.UtcNow;
+                newFilter.EditDate = newFilter.CreateDate;
+
+                //Create file
+                Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"\Filters");
+                FileInfo filterFile = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + @"\Filters\" + newFilter.Name + ".filter");
+                StreamWriter filterText = filterFile.AppendText();
+                filterText.WriteLine("#Description: " + newFilter.Description);
+
+                //Add items to file
+                ItemUserList itemsList = (ItemUserList)Session["ItemsList"] as ItemUserList;
+                if (itemsList != null)
+                {
+                    foreach (var item in itemsList.UsersItems)
+                    {
+                        if (item.Show == true)
+                        {
+                            filterText.WriteLine("Show");
+                        }
+                        else
+                        {
+                            filterText.WriteLine("Hide");
+                        }
+
+                        string output = "";
+                        Type type = item.GetType();
+                        PropertyInfo[] properties = type.GetProperties();
+
+                        foreach (PropertyInfo property in properties)
+                        {
+                            if (property.GetValue(item, null) != null)
+                            {
+                                if (property.Name != "Id" && property.Name != "Show")
+                                {
+                                    output = "    " + (property.Name + " " + property.GetValue(item, null).ToString()).Trim();
+                                    filterText.WriteLine(output);
+                                }
+                            }
+                        }
+                    }
+                }
+                filterText.Close();
+
+                return Json(new { fileName = newFilter.Name }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpGet]
+        public FileResult Download(string file)
+        {
+            file += ".filter";
+            string fullPath = AppDomain.CurrentDomain.BaseDirectory + @"\Filters\" + file;
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName = file,
+                Inline = false,
+            };
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+
+            return File(fullPath, System.Net.Mime.MediaTypeNames.Application.Octet, file);
         }
     }
 }
