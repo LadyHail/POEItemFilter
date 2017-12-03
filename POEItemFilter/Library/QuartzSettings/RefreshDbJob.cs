7572ed9cc0d3ddc22cd1cc5faccdecf4c4e95177
LadyHail <ladyhail@outlook.com>
2017-12-03 12:04:36 +0100

    Quartz implemented.

---
+++ POEItemFilter/Library/QuartzSettings/RefreshDbJob.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace POEItemFilter.Library.QuartzSettings
+{
+    public class RefreshDbJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            GetItemsFromWeb refreshDb = new GetItemsFromWeb();
+        }
+    }
+}
---
+++ POEItemFilter/Library/QuartzSettings/RefreshDbJobScheduler.cs
@@ -0,0 +1,24 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace POEItemFilter.Library.QuartzSettings
+{
+    public class RefreshDbJobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<RefreshDbJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInHours(24)
+                .RepeatForever())
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
